f3dfe0a31f924db6510621751973afe7a5ff0c5b
Dan Murphy <danielmarkmurphy@gmail.com>
2015-10-26 09:08:07 -0700

    Revert "Test for breaks bug"
    This reverts commit de65b9945150b90cd06dfc4ffab00698e99bd2c9.

--- inst/unitTests/runit.cutbugfixes.r
+++
@@ -1,8 +0,0 @@
-test.cut.mondate <- function() {
-  #require(RUnit)
-  x <- mondate.ymd(2015, 1:12)
-  res <- cut(x, breaks = 1)
-  checkTrue(is.null(attr(res, "breaks")))
-  res <- cut(x, breaks = 1, attr.breaks = TRUE)
-  checkTrue(!is.null(attr(res, "breaks")))
-}
